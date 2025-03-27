9fd3bc5491962de1991e507da801ed0122d25e91
khanhtran <kaitran2205@gmail.com>
2025-03-27 19:05:17 +0700

    rebase

--- AntiSwearingChatBox.App/App.xaml.cs
+++ AntiSwearingChatBox.App/App.xaml.cs
@@ -1,60 +1,14 @@
-﻿using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using AntiSwearingChatBox.Repository.Models;
-using AntiSwearingChatBox.Repository.IRepositories;
-using AntiSwearingChatBox.Repository.Repositories;
-using AntiSwearingChatBox.Service.IServices;
-using AntiSwearingChatBox.Service.Services;
-using Microsoft.Extensions.Configuration;
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
 
-namespace AntiSwearingChatBox.App;
-
-public partial class App
+namespace AntiSwearingChatBox.App
 {
-    private ServiceProvider? _serviceProvider;
-    
-    public App()
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
     {
-        var services = new ServiceCollection();
-        ConfigureServices(services);
-        _serviceProvider = services.BuildServiceProvider();
     }
 
-    private void ConfigureServices(IServiceCollection services)
-    {
-        // Configuration
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .Build();
-
-        // DbContext
-        services.AddDbContext<AntiSwearingChatBoxContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-
-        // Main Window
-        services.AddTransient<MainWindow>();
-    }
-
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-
-        var mainWindow = _serviceProvider?.GetService<MainWindow>();
-        if (mainWindow != null)
-        {
-            mainWindow.Show();
-        }
-    }
-
-    protected override void OnExit(ExitEventArgs e)
-    {
-        base.OnExit(e);
-        if (_serviceProvider is IDisposable disposable)
-        {
-            disposable.Dispose();
-        }
-    }
 }
-
