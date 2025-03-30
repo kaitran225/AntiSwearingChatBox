89a6bdb879cc4610d5224c3f46da8d3db96c90bf
khanhtran <kaitran2205@gmail.com>
2025-03-30 21:43:58 +0700

    save

--- AntiSwearingChatBox.AI/GeminiSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace AntiSwearingChatBox.AI
-{
-    public class GeminiSettings
-    {
-        public string ApiKey { get; set; } = "AIzaSyD9Odq-PkFqA2HHYsR86EEhPbM85eHF2Sw";
-        public string ModelName { get; set; } = "gemini-1.5-pro";
-    }
-} 
